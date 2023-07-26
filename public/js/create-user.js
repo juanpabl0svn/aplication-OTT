bd4da82f5736299b080c24c9efc6a68b9c117803
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-07-26 11:04:11 -0500

    tenemos la creacion y autenticacion de usuarios

--- public/js/create-user.js
+++ public/js/create-user.js
@@ -1,8 +1,44 @@
-const $ = (el) => document.querySelector(el)
+const $ = (el) => document.querySelector(el);
 
+const URL_BACKEND = "http://localhost:5000";
 
-$('form').addEventListener('submit',(e)=>{
+function getMermershipTime() {
+  const fecha = new Date();
+  const day = fecha.getDate();
+  const month = fecha.getMonth();
+  const year = fecha.getFullYear();
+  const currentDate = `${year}/${month}/${day}`;
+  const next = `${year + 1}/${month}/${day}`;
+  return [currentDate, next];
+}
+$("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const data = Object.fromEntries(new FormData(e.target))
-  console.log(data)
-})
+  const data = Object.fromEntries(new FormData(e.target));
+  if (data.membership == 0) {
+    return alert("Please select a membership");
+  }
+  if (data.password != data.rpassword) {
+    return alert("Passwords are not the same");
+  }
+
+  const [current, next] = getMermershipTime();
+
+  data["startMerbership"] = new Date(current);
+  data["endMerbership"] = new Date(next);
+
+  console.log(data);
+
+  const request = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body : JSON.stringify(data)
+  }
+
+  const req = await fetch(URL_BACKEND+'/create',request)
+
+  const res = await req.json()
+
+  console.log(res)
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,27 @@
-fetch('http://localhost:5000/db.json')
-.then(res => res.json())
-.then(res => console.log(res))
+const $ = (el) => document.querySelector(el);
+
+const URL_BACKEND = "http://localhost:5000";
+
+$("form").addEventListener("submit", async(e) => {
+  e.preventDefault();
+
+  const data = Object.fromEntries(new FormData(e.target))
+
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+
+  console.log(data)
+
+  const req = await fetch(URL_BACKEND +'/auth',request)
+  const res = await req.json()
+
+  console.log(res)
+
+
+
+});
