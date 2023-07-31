6cbba507ea63b221cd45b56bf4e8145ca8489062
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-07-31 00:03:50 -0500

    dimos toques finales

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,35 +1,6 @@
-const $ = (el) => document.querySelector(el)
+const $ = (el) => document.querySelector(el);
 
-const URL_BACKEND = 'http://localhost:5000'
-
-function logOut() {
-  document.cookie.split(";").forEach(function (cookie) {
-    document.cookie = cookie
-      .replace(/^ +/, "")
-      .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-  });
-  location.href = '/'
-}
-
-
-
-
-$('#user').addEventListener('click',()=>{
-  $('aside').classList.toggle('hidden')
-})
-
-async function deleteAccount(){
-  const cookie = document.cookie
-  const token = cookie.slice(6,cookie.length)
-  console.log(token)
-  const req = await fetch(URL_BACKEND + `/delete/${token}`,{method: 'DELETE'})
-
-  if (req.status !== 200) return alert('Nel')
-
-  alert('Cuenta borrada')
-  logOut()
-}
-
+const URL_BACKEND = "http://localhost:5000";
 
 function logOut() {
   document.cookie.split(";").forEach(function (cookie) {
@@ -37,5 +8,34 @@ function logOut() {
       .replace(/^ +/, "")
       .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
   });
-  location.href = '/'
+  location.href = "/";
+}
+
+$("#user").addEventListener("click", () => {
+  $("aside").classList.toggle("hidden");
+});
+
+async function deleteAccount() {
+  const result = await Swal.fire({
+    title: "¿Estas seguro que quieres borrar tu cuenta?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: "Borrar",
+  });
+
+  if (!result.isCanceled) {
+    return
+  }
+
+
+  const cookie = document.cookie;
+  const token = cookie.slice(6, cookie.length);
+  console.log(token);
+  const req = await fetch(URL_BACKEND + `/delete/${token}`, {
+    method: "DELETE",
+  });
+
+  if (req.status !== 200) return alert("Nel");
+
+  logOut();
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -111,16 +111,31 @@ router.get(
       </div>
     </header>
     <aside id="user-options" class="hidden">
-      <input type="button" value="Ajustes" onclick="location.href='/settings'">
-      <input type="button" value="Log out" onclick="logOut()">
+      <input type="button" value="Inicio" onclick="location.href = '/page'" />
+      <input
+        type="button"
+        value="Ajustes"
+        onclick="location.href='/settings'"
+      />
+      <input type="button" value="Log out" onclick="logOut()" />
     </aside>
   <main>
   <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </main>
   <footer>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse quibusdam,
-      illum est qui voluptas soluta hic earum incidunt, quas explicabo ex alias
-      quo. Itaque dolore eaque odit minima inventore reiciendis.
+      <div>
+        <a
+          href="https://www.flaticon.es/iconos-gratis/palomitas-de-maiz"
+          title="palomitas de maiz iconos"
+          >Palomitas de maiz iconos creados por Freepik - Flaticon</a
+        >
+        <a
+          href="https://www.flaticon.es/iconos-gratis/usuario"
+          title="usuario iconos"
+          >Usuario iconos creados por Icon Mart - Flaticon</a
+        >
+      </div>
+      <div id="copyright">Copyright (c) 2003 Vajuri</div>
     </footer>
   
 </body>
@@ -141,9 +156,8 @@ router.get(
     return res.redirect("/");
   },
   function (req, res) {
-    res.sendFile(path('settings.html'));
+    res.sendFile(path("settings.html"));
   }
 );
-
 
 module.exports = router;
